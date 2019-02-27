61b095fb2de1afd55b145524ec1315d2549063c2
Onur YARTAŞI <onuryartasi@live.com>
2019-02-27 16:24:17 +0300

    refactoring code

--- cmd/root.go
+++ cmd/root.go
@@ -31,13 +31,7 @@ var contextFlag string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "ctxm",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Easy to use kubernetes contexts",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
