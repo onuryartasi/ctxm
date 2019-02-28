f020309e2c8d1d2316906c3732b38ece51d65f61
Onur YARTAŞI <onuryartasi@live.com>
2019-02-28 10:07:46 +0300

    Adding current context print

--- cmd/root.go
+++ cmd/root.go
@@ -27,6 +27,7 @@ import (
 
 var cfgFile string
 var contextFlag string
+var current bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -36,7 +37,13 @@ var rootCmd = &cobra.Command{
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
 	Run: func(cmd *cobra.Command, args []string) {
-		ChangeContext(args...)
+		if current {
+			context := util.GetCurrentContext()
+			fmt.Println(context)
+		} else {
+			ChangeContext(args...)
+		}
+
 	},
 }
 
@@ -72,6 +79,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
+	rootCmd.Flags().BoolVarP(&current, "current", "c", false, "Get Current COntext")
 	rootCmd.Flags().StringVarP(&contextFlag, "name", "n", "", "Change the context")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
